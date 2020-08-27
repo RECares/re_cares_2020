7a9bef683af62bc749da8db7e84ddbf24e5a2b78
Jared Payne <jaredmpayne@gmail.com>
2020-08-27 00:53:04 -0400

    Add database seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+
+site_modules = JSON.parse(File.read('db/site_modules.json'))["site_modules"]
+site_modules.each do |site_module|
+  the_site_module = SiteModule.create(name: site_module["name"])
+  site_module["questions"].each do |question|
+    the_question = Question.create(site_module: the_site_module, body: question["body"])
+    question["answers"].each do |answer|
+      the_answer = Answer.create(question: the_question, body: answer["body"])
+    end
+  end
+end
