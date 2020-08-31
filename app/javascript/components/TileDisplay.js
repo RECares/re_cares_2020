5ef0f20993a406adc6d16edcf5d8bfa3123b7489
english13 <spanthar@calpoly.edu>
2020-08-31 16:06:29 -0700

    first tiledisplay component and csswq

---
+++ app/javascript/components/TileDisplay.js
@@ -0,0 +1,29 @@
+import React from "react";
+//import { Link } from "react-router-dom";
+import "./TileDisplay.css";
+//import TweetImage from "./Common/TweetImage"
+class TileDisplay extends React.Component {
+    constructor(props) {
+  super(props);
+
+  //this.createTasks = this.createTasks.bind(this);
+}
+render() {
+ 
+    return (
+    <>
+      <div className="button1">
+        </div>
+        <div className="button2">
+        </div>
+        <div className="button3">
+         </div>
+        <div className="button4">
+        
+        </div>
+    </>
+    );
+  }
+};
+
+export default TileDisplay;
