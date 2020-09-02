a51029b257928dba690d55e126d7820011a986d8
Greg Bisbjerg <gbisbjerg@gmail.com>
2020-09-02 13:59:50 -0700

    Fixed formating

--- app/javascript/components/NavBar/NavBarLoggedIn.js
+++ app/javascript/components/NavBar/NavBarLoggedIn.js
@@ -3,43 +3,45 @@ import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
 import "./NavBar.css";
 
+// NavBar to be used when there is a user logged in. 
+// "npm install --save react-router-dom" may need to be run
 class NavBarLoggedIn extends React.Component {
-    render() {
-        return (
-            <div className="nav-bar">
-               <div className="nav-logo">
-                  <div className="nav-logo1">
-                     Best
-                  </div>
-                  <div className="nav-logo2">
-                     Practices
-                  </div>
+   render() {
+      return (
+         <div className="nav-bar">
+            <div className="nav-logo">
+               <div className="nav-logo1">
+                  Best
                </div>
-
-               <div>
-                  <Link to="/SignOut" className="routingButton" role="button"> 
-                     <div> Sign Out </div>
-                  </Link>
-                  <div className="routingButton"> | </div>
-                  <Link to="/Profile" className="routingButton" role="button"> 
-                     <div> Profile </div>
-                  </Link>
-                  <div className="routingButton"> | </div>
-                  <Link to="/Resources" className="routingButton" role="button"> 
-                     <div> Resources </div>
-                  </Link>
-                  <div className="routingButton"> | </div>
-                  <Link to="/AboutUS" className="routingButton" role="button"> 
-                     <div> About US </div>
-                  </Link>
-                  <div className="routingButton"> | </div>
-                  <Link to="/Home" className="routingButton" role="button"> 
-                     <div> Home </div>
-                  </Link>
-  		      	 </div>
+               <div className="nav-logo2">
+                  Practices
+               </div>
             </div>
-        )
-    }
+
+            <div>
+               <Link to="/SignIn" className="routingButton" role="button"> 
+                  <div> Sign Out </div>
+               </Link>
+               <div className="routingButton"> | </div>
+               <Link to="/Profile" className="routingButton" role="button"> 
+                  <div> Profile </div>
+               </Link>
+               <div className="routingButton"> | </div>
+               <Link to="/Resources" className="routingButton" role="button"> 
+                  <div> Resources </div>
+               </Link>
+               <div className="routingButton"> | </div>
+               <Link to="/AboutUS" className="routingButton" role="button"> 
+                  <div> About US </div>
+               </Link>
+               <div className="routingButton"> | </div>
+               <Link to="/Home" className="routingButton" role="button"> 
+                  <div> Home </div>
+               </Link>
+             </div>
+         </div>
+      )
+   }
 }
 
 export default NavBarLoggedIn;
