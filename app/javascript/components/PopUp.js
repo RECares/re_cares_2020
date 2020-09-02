954e7118d07ae082606cc43257cef22599ede444
english13 <spanthar@calpoly.edu>
2020-09-02 11:24:18 -0700

    Initial popup components - can display

---
+++ app/javascript/components/PopUp.js
@@ -0,0 +1,22 @@
+import React from "react";
+//import { Link } from "react-router-dom";
+import "./PopUp.css";
+//import TweetImage from "./Common/TweetImage"
+class PopUp extends React.Component {
+    constructor(props) {
+  super(props);
+
+  //this.createTasks = this.createTasks.bind(this);
+}
+render() {
+ 
+    return (
+    <>
+      <div className="PopUp">
+        </div>
+    </>
+    );
+  }
+};
+
+export default PopUp;
